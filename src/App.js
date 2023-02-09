06a2432865eda113162b4a25c66074f469c44660
AhmedSEHILI <ka_sehili@esi.dz>
2023-02-09 23:52:29 +0100

    particles added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Particlestest from './Particlestest';
+
+function App() {
+  return (
+    <div className="App">
+      <Particlestest />
+    </div>
+  );
+}
+
+export default App;
